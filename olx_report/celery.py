f464f88348f547b144a6f064350a2ebc843dfe7a
Gurgen <gurgen.abramyan.1995@gmail.com>
2019-04-25 15:57:22 +0300

    first

---
+++ olx_report/celery.py
@@ -0,0 +1,10 @@
+import os
+from celery import Celery
+ 
+ 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'olx_report.settings')
+ 
+app = Celery('olx_report')
+app.config_from_object('django.conf:settings')
+ 
+app.autodiscover_tasks()
